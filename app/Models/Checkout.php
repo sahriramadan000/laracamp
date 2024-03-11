0befa2571c777b0bbb0e9e143c3a0aa3c6d94fad
Sahri Ramadan <sahriramadan000@gmail.com>
2024-03-11 07:46:46 +0700

    init midtrans

--- app/Models/Checkout.php
+++ app/Models/Checkout.php
@@ -9,12 +9,12 @@ class Checkout extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['user_id', 'camp_id', 'card_number', 'expired', 'cvc', 'is_paid'];
+    protected $fillable = ['user_id', 'camp_id','payment_status', 'midtrans_url', 'midtrans_booking_code'];
 
-    public function setExpiredAttribute($value)
-    {
-        $this->attributes['expired'] = date('Y-m-t', strtotime($value));
-    }
+    // public function setExpiredAttribute($value)
+    // {
+    //     $this->attributes['expired'] = date('Y-m-t', strtotime($value));
+    // }
 
     public function Camp()
     {
